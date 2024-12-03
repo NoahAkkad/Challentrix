6483c45628ceb721a0733f829ebe45edbcdd9968
HaidarGhanem <haidar23ghanem@gmail.com>
2024-12-03 16:28:19 +0300

    Postgre working correctly

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,22 @@
-require('dotenv').config()
-const { sequelize, User } = require('./models');
-
 const express = require('express')
+const { Sequelize } = require('sequelize')
+const config = require('./config/config.json')[process.env.NODE_ENV || 'development']
+const db = require('./models')
 
 const server = express()
 const PORT = process.env.PORT || 3000
 
-sequelize.authenticate()
-    .then(() => console.log('Connection has been established successfully.'))
-    .catch(err => console.error('Unable to connect to the database:', err))
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+})
 
 server.use(express.json())
-server.use(express.urlencoded({extended:true}))
 
 server.use('/test', require('./tests/user_crud'))
 
-server.listen(PORT, () => {
-    console.log(`server is running`)} )
+db.sequelize.sync().then(() => {
+    server.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`)
+    })
+})
