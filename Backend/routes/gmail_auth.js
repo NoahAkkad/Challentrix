511fbe89e293525ca9f66464d002119ae66fb4c2
HaidarGhanem <haidar23ghanem@gmail.com>
2025-01-04 00:08:18 +0300

    full gmail authentication

---
+++ Backend/routes/gmail_auth.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const passport = require('passport')
+//for gmail auth requirement
+require('../middleware/passport')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('<a href="/auth/google">Sign In with Google</a>')
+})
+
+router.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+)
+
+router.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+    res.send(`Hello, ${req.user.displayName}`) 
+})
+
+router.get('/logout', (req, res) => {
+    req.logout(err => {
+    if (err) return next(err)
+    res.redirect('/')
+    })
+})
+
+module.exports = router
